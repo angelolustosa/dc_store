7036c6634fca0637a9c72fd646c818b301363538
Angelo <prataadvg@gmail.com>
2023-06-13 19:27:38 -0300

    footer

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Header } from '../header/Header'
+import { Footer } from '../footer/Footer'
+
+export const Home = () => {
+    return (
+        <div>
+            <Header />
+            {/* <Menu active="home" />
+
+            <Banner />
+
+            <TrendingCollection /> */}
+            <Footer />
+        </div>
+    )
+}
